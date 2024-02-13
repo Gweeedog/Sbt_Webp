8aa560e25881977cf035fc9e42176e77a05b6e1b
Guido Grumetti <guidogrumetti@outlook.it>
2024-02-13 18:08:34 +0900

    added tssk request form

--- sbt_app/forms.py
+++ sbt_app/forms.py
@@ -1,6 +1,8 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import task
+from django.forms import ModelForm
 
 class RegisterForm(UserCreationForm):
     User._meta.get_field('username').validators[1].limit_value = 15
@@ -12,3 +14,14 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "indirizzo", "numero_di_telefono", "password1", "password2"]
+
+class TaskRequestForm(ModelForm):
+    data_limite = forms.DateField(required=True, widget=forms.DateInput(attrs={"type": "date"}))
+    orario_limite = forms.TimeField(required=True, widget=forms.TimeInput(attrs={"type": "time"}))
+    class Meta:
+        model = task
+        fields = ("title", "target", "data_limite", "orario_limite")
+        labels = {
+            "title": "Contenuto",
+            "target": "Incaricato",
+        }
--- sbt_app/views.py
+++ sbt_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import task
-from .forms import RegisterForm
+from .forms import RegisterForm, TaskRequestForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 import datetime
@@ -51,5 +51,28 @@ def logout_view(request):
 def store(request):
     return render(request, "store.html")
 
+@login_required(login_url="/login")
 def task_request(request):
-    return render(request, "task_request.html")
+    if request.method == "POST":
+        form = TaskRequestForm(request.POST)
+        if form.is_valid():
+            newtask = task()
+            newtask.submission_date = datetime.datetime.now(tz=ZoneInfo("Europe/Rome"))
+            newtask.author = request.user
+            newtask.target = form.cleaned_data["target"]
+            newtask.title = form.cleaned_data["title"]
+            date_value = form.cleaned_data["data_limite"]
+            time_value = form.cleaned_data["orario_limite"]
+            newtask.datetime_limit = datetime.datetime(
+                year=date_value.year,
+                month=date_value.month,
+                day=date_value.day,
+                hour=time_value.hour,
+                minute=time_value.minute,
+                second=00
+            )
+            newtask.save()
+            return redirect("tasks")
+    else:
+        form = TaskRequestForm()
+    return render(request, "task_request.html", {"form": form})
